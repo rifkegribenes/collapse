117a5b2c0311e4de25c6edda4ac06743a8de778b
Sarah Schneider <rifkegribenes@gmail.com>
2018-07-27 23:30:49 -0700

    Update stock.ctrl.js

--- app/controllers/stock.ctrl.js
+++ app/controllers/stock.ctrl.js
@@ -5,20 +5,14 @@ const https = require('https');
 const handleError = (res, statusCode) => {
   const status = statusCode || 500;
   return (err) => {
-    console.log('8');
-    console.log(err);
     res.status(status).send(err);
   };
 }
 
 const handleResponse = (res, statusCode) => {
-      console.log('15');
   const status = statusCode || 200;
   return (entity) => {
-    console.log('17');
-    console.log(entity);
     if (entity) {
-      console.log('20');
       res.status(status).json(entity);
     }
   };
@@ -43,14 +37,12 @@ const updateDB = (updates) => {
   };
 }
 
-const removeEntity = (req, res, next) => {
-  console.log('42');
+const removeEntity = (res) => {
   return (entity) => {
     if (entity) {
       return entity.remove()
         .then(() => {
-          console.log('47');
-          next();
+          res.status(204).end();
         });
     }
   };
@@ -58,12 +50,8 @@ const removeEntity = (req, res, next) => {
 
 // get all stocks
 exports.getAllStocks = (req, res) => {
-  console.log('60');
   Stock.find()
-    .then(() => {
-      console.log('63');
-      handleResponse(res);
-    })
+    .then(handleResponse(res))
     .catch(handleError(res));
 }
 
@@ -107,8 +95,8 @@ exports.getOneStock = (req, res) => {
   }
 
 // add stock to mongo
-exports.addStock = (req, res, next) => {
-  console.log('110');
+exports.addStock = (req, res) => {
+
   if (!req.params.stock) {
     return res.status(400).json({ message: 'Stock not found.' });
   }
@@ -122,40 +110,30 @@ exports.addStock = (req, res, next) => {
 
     res2.on('end', () => {
       data = JSON.parse(data);
-      console.log('124');
 
-      // res
-      //   .status(res2.statusCode)
-      //   .end();
+      res
+        .status(res2.statusCode)
+        .end();
 
       if (parseInt(res2.statusCode / 100) === 2) {
         const code = data.dataset.dataset_code;
-        console.log('133');
+
         // look for stock in DB by code
         Stock.find({ code })
           .then((stock) => {
-            console.log('1373');
+
             // if stock already exists in DB, return
             if (stock.length) {
-                console.log('140');
-              return next(req, res2);
+              return;
             }
 
             // otherwise, create new record in mongo
             Stock.create({
               name: data.dataset.name,
               code: data.dataset.dataset_code
-            }).then(() => {
-              console.log('149');
-              next(req, res2);
             });
 
-
           });
-      } else {
-        console.log('155');
-        console.log(`status: ${res2.statusCode}`);
-        handleError(res2);
       }
     });
   }).on('error', (err) => {
@@ -176,12 +154,9 @@ exports.updateStock = (req, res) => {
 }
 
 // delete stock
-exports.removeStock = (req, res, next) => {
+exports.removeStock = (req, res) => {
   Stock.findById(req.params.stock)
     .then(handleNotFound(res))
-    .then(removeEntity(req, res, next))
-    .catch((err) => {
-      console.log(err)
-      handleError(res)
-    });
+    .then(removeEntity(res))
+    .catch(handleError(res));
 }
